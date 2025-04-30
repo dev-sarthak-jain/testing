cb69af832ddf6845205d36ba58e1c7d36a9b3999
Sarthak <93002499+dev-sarthak-jain@users.noreply.github.com>
2025-04-30 11:12:54 +0530

    Update appv4.py

--- appv4.py
+++ appv4.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 import streamlit as st
 
 load_dotenv()
-gemini_api_key = gemini_api_key = st.secrets["GEMINI_API_KEY"]
+gemini_api_key = st.secrets["GEMINI_API_KEY"]
 
 st.title(gemini_api_key[0:7])
